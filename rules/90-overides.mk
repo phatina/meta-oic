rzr/meta-yocto-demos

build: include bsp file by default
Change-Id: Ie53311376896102b3a1c58f3182f7a9ad0497e18
Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

rules/90-overides.mk
@@ -2,7 +2,7 @@
 # Author: Philippe Coval <philippe.coval@osg.samsung.com>
 # ex: set tabstop=4 noexpandtab:
 
--include rules/config/bsp/${BSP}/config.mk
+-include rules/config/machine/${MACHINE}/config.mk
 
 #rule/overide/all:
 #	$(warning # log: skip building ${@F}: ${ARG})

phatina/meta-oic

rules/90-overides.mk
@@ -2,7 +2,7 @@
 # Author: Philippe Coval <philippe.coval@osg.samsung.com>
 # ex: set tabstop=4 noexpandtab:
 
--include rules/config/bsp/${BSP}/config.mk
+-include rules/config/bsp/${bsp}/config.mk
 
 #rule/overide/all:
 #	$(warning # log: skip building ${@F}: ${ARG})